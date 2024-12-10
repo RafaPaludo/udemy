bf21883a4dfd4fba29870aec7888b3ace56c9ebe
RafaPaludo <rafael.paludo@franq.com.br>
2024-12-10 14:25:46 -0300

    feat: instalando o mysql e criando a conexão com o db

--- Nodejs/08_NODE_+_MY_SQL/1_instalacao_mysql/index.js
+++ Nodejs/08_NODE_+_MY_SQL/1_instalacao_mysql/index.js
@@ -4,28 +4,25 @@ const mysql = require("mysql")
 
 const app = express()
 
-app.engine('handlebars', exphbs.engine)
-app.set('view engine', 'handlebars')
+app.use(express.json())
 
-app.use(express.static('public'))
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
   res.render('home')
 })
 
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "nodemysql"
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'nodemysql'
 })
 
 conn.connect((err) => {
-  if (err) {
-    console.log(err)
-  }
+  if (err) throw err;
 
-  console.log("Conectou ao mysql")
-
+  console.log('Conectou ao MySQL!')
   app.listen(3000)
 })
